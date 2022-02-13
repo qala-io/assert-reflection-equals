432a407cd275dd68259249279bea6dcb07ba7be4
iuliia_gorshkova <gorshkovayulia93@gmail.com>
2022-02-13 19:53:03 +0300

    Issue #1, #2 1. Fix .pom files and validation, 3. Add more tests and test objects

---
+++ core/src/main/java/io/elsci/assertreflectionequals/ReflectionAssert.java
@@ -0,0 +1,50 @@
+package io.elsci.assertreflectionequals;
+
+import java.lang.reflect.Field;
+
+public class ReflectionAssert {
+    public static void assertReflectionEquals(Object expected, Object actual) throws IllegalAccessException {
+        if (expected == null && actual == null) {
+            return;
+        }
+        if (expected == null || actual == null) {
+            throwAssertionError("Objects are not equal since one of them is null");
+        }
+        StringBuilder errorMessage = new StringBuilder();
+
+        Class<?> expectedClass = expected.getClass();
+        Class<?> actualClass = actual.getClass();
+
+        if(expectedClass != actualClass) {
+            throwAssertionError("Objects are not equal since their classes are different");
+        }
+
+        Field[] expectedFields = expectedClass.getDeclaredFields();
+        Field[] actualFields = actualClass.getDeclaredFields();
+
+        for (int i = 0; i < expectedFields.length; i++) {
+            expectedFields[i].setAccessible(true);
+            actualFields[i].setAccessible(true);
+
+            if(!expectedFields[i].get(expected).equals(actualFields[i].get(actual))) {
+                buildErrorMessage(expectedFields[i].get(expected), expectedFields[i].getName(),
+                        actualFields[i].get(actual), actualFields[i].getName(), errorMessage);
+            }
+        }
+
+        if (errorMessage.length() != 0) {
+            throwAssertionError(errorMessage.toString());
+        }
+
+    }
+
+    private static void buildErrorMessage(Object expectedValue, String expectedName,
+                                          Object actualValue, String actualName, StringBuilder errorMessage) {
+        errorMessage.append("The ").append(expectedName).append(" of expected object is ").append(expectedValue).
+                append(", but actual ").append(actualName).append(" is ").append(actualValue).append("\n");
+    }
+
+    private static void throwAssertionError(String message) {
+        throw new AssertionError(message);
+    }
+}
--- core/src/test/java/io/elsci/assertreflectionequals/Person.java
+++ core/src/test/java/io/elsci/assertreflectionequals/Person.java
@@ -1,4 +1,4 @@
-package Assertion;
+package io.elsci.assertreflectionequals;
 
 public class Person {
     long id;
