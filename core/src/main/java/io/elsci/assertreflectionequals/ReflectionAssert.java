47b1c6a0c11a46287c0d9d53c4c24e28d8c92951
iuliia_gorshkova <gorshkovayulia93@gmail.com>
2022-02-20 12:11:52 +0300

    Issue #4 Implement excludeFields(String ... fieldNames) method + add corresponding tests

--- core/src/main/java/io/elsci/assertreflectionequals/ReflectionAssert.java
+++ core/src/main/java/io/elsci/assertreflectionequals/ReflectionAssert.java
@@ -1,9 +1,15 @@
 package io.elsci.assertreflectionequals;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class ReflectionAssert {
-    public static void assertReflectionEquals(Object expected, Object actual) {
+    private HashSet<String> excludedFields;
+
+    public void assertReflectionEquals(Object expected, Object actual) {
         if (expected == null && actual == null) {
             return;
         }
@@ -14,18 +20,15 @@ public class ReflectionAssert {
 
         Class<?> expectedClass = expected.getClass();
         Class<?> actualClass = actual.getClass();
-
         if(expectedClass != actualClass) {
-            throwAssertionError("Objects are not equal since their classes are different");
+            throwAssertionError("Expected " + expectedClass + ", but actual " + actualClass);
         }
 
-        Field[] expectedFields = expectedClass.getDeclaredFields();
-        Field[] actualFields = actualClass.getDeclaredFields();
-
+        Field[] expectedFields = getProperFields(expectedClass.getDeclaredFields());
+        Field[] actualFields = getProperFields(actualClass.getDeclaredFields());
         for (int i = 0; i < expectedFields.length; i++) {
             expectedFields[i].setAccessible(true);
             actualFields[i].setAccessible(true);
-
             try {
                 if (!expectedFields[i].get(expected).equals(actualFields[i].get(actual))) {
                     buildErrorMessage(expectedFields[i].get(expected), expectedFields[i].getName(),
@@ -39,16 +42,52 @@ public class ReflectionAssert {
         if (errorMessage.length() != 0) {
             throwAssertionError(errorMessage.toString());
         }
-
     }
 
-    private static void buildErrorMessage(Object expectedValue, String expectedName,
+    public ReflectionAssert excludeFields(String ... fieldNames) {
+        excludedFields = new HashSet<>(Arrays.asList(fieldNames));
+        return this;
+    }
+
+    // Get proper array of fields in case some fields were excluded
+    private Field[] getProperFields(Field[] objectFields) {
+        if (excludedFields == null) {
+            return objectFields;
+        }
+        verifyExcludedFields(excludedFields, objectFields);
+        ArrayList<Field> fields = new ArrayList<>();
+        for (Field objectField : objectFields) {
+            objectField.setAccessible(true);
+            if (!excludedFields.contains(objectField.getName())) {
+                fields.add(objectField);
+            }
+        }
+        return fields.toArray(new Field[0]);
+    }
+
+    // Verify if excluded field name belongs to the specified object
+    private void verifyExcludedFields(HashSet<String> excludedFields, Field[] objectFields) {
+        ArrayList<String> fields = new ArrayList<>();
+        for (Field objectField : objectFields) {
+            objectField.setAccessible(true);
+            fields.add(objectField.getName());
+        }
+        Iterator<String> iterator = excludedFields.iterator();
+        if (iterator.hasNext()) {
+            String name = iterator.next();
+            if(!fields.contains(name)) {
+                throw new IllegalArgumentException(name + " field is not property of specified object");
+            }
+        }
+    }
+
+    private void buildErrorMessage(Object expectedValue, String expectedName,
                                           Object actualValue, String actualName, StringBuilder errorMessage) {
         errorMessage.append("The ").append(expectedName).append(" of expected object is ").append(expectedValue).
                 append(", but actual ").append(actualName).append(" is ").append(actualValue).append("\n");
     }
 
-    private static void throwAssertionError(String message) {
+    private void throwAssertionError(String message) {
         throw new AssertionError(message);
     }
 }
--- core/src/test/java/io/elsci/assertreflectionequals/ReflectionAssertTest.java
+++ core/src/test/java/io/elsci/assertreflectionequals/ReflectionAssertTest.java
@@ -10,34 +10,34 @@ public class ReflectionAssertTest {
     public void twoObjectsAreEqualIfAllValuesOfTheirPropertiesAreEqual() {
         Person person = new Person(150245871L, (short) 25, 50.1007d, 164, (byte) 0b10, 69f);
         Person person2 = new Person(150245871L, (short) 25, 50.1007d, 164, (byte) 0b10, 69f);
-        ReflectionAssert.assertReflectionEquals(person, person2);
+        new ReflectionAssert().assertReflectionEquals(person, person2);
     }
 
     @Test
     public void objectIsEqualToItself() {
         Person person = new Person(150245871L, (short) 25, 50.1007d, 164, (byte) 0b10, 69f);
-        ReflectionAssert.assertReflectionEquals(person, person);
+        new ReflectionAssert().assertReflectionEquals(person, person);
     }
 
     @Test
     public void objectsAreEqualIfBothAreNull() {
         Person person = null;
         Person person2 = null;
-        ReflectionAssert.assertReflectionEquals(person, person2);
+        new ReflectionAssert().assertReflectionEquals(person, person2);
     }
 
     @Test
     public void possibleToCompareObjectsWithPrivateFields() {
         Animal animal = new Animal(150245871L, (short) 25, 50.1007d, 164);
         Animal animal2 = new Animal(150245871L, (short) 25, 50.1007d, 164);
-        ReflectionAssert.assertReflectionEquals(animal, animal2);
+        new ReflectionAssert().assertReflectionEquals(animal, animal2);
     }
 
     @Test
     public void possibleToCompareObjectsWithProtectedFields() {
         Plant plant = new Plant(150245871L, (short) 3, 50);
         Plant plant2 = new Plant(150245871L, (short) 3, 50);
-        ReflectionAssert.assertReflectionEquals(plant, plant2);
+        new ReflectionAssert().assertReflectionEquals(plant, plant2);
     }
 
     @Test
@@ -45,7 +45,7 @@ public class ReflectionAssertTest {
         Person person = null;
         Person person2 = new Person(150245871L, (short) 25, 50.1007d, 164, (byte) 0b10, 69f);
         AssertionError e = assertThrows(AssertionError.class,
-                () -> ReflectionAssert.assertReflectionEquals(person, person2));
+                () -> new ReflectionAssert().assertReflectionEquals(person, person2));
         assertEquals("Objects are not equal since one of them is null", e.getMessage());
     }
 
@@ -54,7 +54,7 @@ public class ReflectionAssertTest {
         Person person = new Person(150245871L, (short) 25, 50.1007d, 164, (byte) 0b10, 69f);
         Person person2 = null;
         AssertionError e = assertThrows(AssertionError.class, () ->
-                ReflectionAssert.assertReflectionEquals(person, person2));
+                new ReflectionAssert().assertReflectionEquals(person, person2));
         assertEquals("Objects are not equal since one of them is null", e.getMessage());
     }
 
@@ -63,8 +63,9 @@ public class ReflectionAssertTest {
         Person person = new Person(150245871L, (short) 25, 50.1007d, 164, (byte) 0b10, 69f);
         Animal animal = new Animal(150245871L, (short) 25, 50.1007d, 164);
         AssertionError e = assertThrows(AssertionError.class, () ->
-                ReflectionAssert.assertReflectionEquals(person, animal));
-        assertEquals("Objects are not equal since their classes are different", e.getMessage());
+                new ReflectionAssert().assertReflectionEquals(person, animal));
+        assertEquals("Expected class io.elsci.assertreflectionequals.Person, " +
+                "but actual class io.elsci.assertreflectionequals.Animal", e.getMessage());
     }
 
     @Test
@@ -72,12 +73,39 @@ public class ReflectionAssertTest {
         Person person = new Person(150245872L, (short) 30, 55.1007d, 170, (byte) 0b0111_1111, 100f);
         Person person2 = new Person(150245871L, (short) 25, 50.1007d, 164, (byte) 0b10, 69f);
         AssertionError e = assertThrows(AssertionError.class, () ->
-                ReflectionAssert.assertReflectionEquals(person, person2));
+                new ReflectionAssert().assertReflectionEquals(person, person2));
         assertEquals("The id of expected object is 150245872, but actual id is 150245871\n" +
                 "The age of expected object is 30, but actual age is 25\n" +
                 "The weight of expected object is 55.1007, but actual weight is 50.1007\n" +
                 "The height of expected object is 170, but actual height is 164\n" +
                 "The shoeSize of expected object is 127, but actual shoeSize is 2\n" +
                 "The waist of expected object is 100.0, but actual waist is 69.0\n", e.getMessage());
+    }
+
+    @Test
+    public void objectsAreEqualIfFieldsWithDifferentValuesWereExcluded() {
+        Person person = new Person(150245871L, (short) 25, 50.1007d, 164, (byte) 0b10, 69f);
+        Person person2 = new Person(150245871L, (short) 25, 50.1007d, 164, (byte) 0b10, 70f);
+        new ReflectionAssert().excludeFields("id", "waist").assertReflectionEquals(person, person2);
+    }
+
+    @Test
+    public void objectsAreEqualIfAllFieldsWereExcluded() {
+        Person person = new Person(150245871L, (short) 25, 50.1007d, 164, (byte) 0b10, 69f);
+        Person person2 = new Person(150245872L, (short) 25, 50.1007d, 164, (byte) 0b10, 70f);
+        new ReflectionAssert().
+                excludeFields("id", "age", "weight", "height", "shoeSize", "waist").
+                assertReflectionEquals(person, person2);
+    }
+
+    @Test
+    public void returnsIllegalArgumentExceptionIfSpecifiedFieldIsNotPropertyOfSpecifiedObject() {
+        Person person = new Person(150245871L, (short) 25, 50.1007d, 164, (byte) 0b10, 69f);
+        Person person2 = new Person(150245872L, (short) 25, 50.1007d, 164, (byte) 0b10, 69f);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new ReflectionAssert().
+                excludeFields("id", "test").
+                assertReflectionEquals(person, person2));
+        assertEquals("test field is not property of specified object",
+                "test field is not property of specified object");
     }
 }
